280bfdabd15822094ec082d979aee0313ec80849
Will Morrison <wmorrison@frontlineed.com>
2020-08-26 17:39:34 -0400

    updates

--- create.ps1
+++ create.ps1
@@ -3,13 +3,18 @@ param (
     [string]$QaRepo = "C:\Users\wmorrison\source\fcqa"
 )
 
-$branch=git rev-parse --abbrev-ref HEAD; 
+$branch = git rev-parse --abbrev-ref HEAD;
 
-npm run build; 
+npm run build;
 
-mkdir $QaRepo/$branch; 
-cp build/* $QaRepo/$branch; 
-cd $QaRepo/; git add .; 
-git commit -m "updates"; 
-git push origin master; 
+if (!(Test-Path $QaRepo/$branch)) {
+    mkdir $QaRepo/$branch;
+}
+
+cp build/* $QaRepo/$branch;
+
+cd $QaRepo/; git add .; git commit -m "updates"; git push origin master;
+
+cd $SourceRepo
+
 Write-Host "Deployed at https://wmorrison-frontlineed.github.io/fcqa/$branch" -ForegroundColor Blue
