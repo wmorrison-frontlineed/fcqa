00feb8f6bf275ac8a3f98fc82d0d68e5fb3ae9bd
Will Morrison <wmorrison@frontlineed.com>
2020-08-28 15:56:18 -0400

    update script to check branch always

--- FCTools/PushPreview.ps1
+++ FCTools/PushPreview.ps1
@@ -8,28 +8,29 @@
         [string]$QaRepo = "C:\Users\wmorrison\source\fcqa",
         [string]$branch = (git rev-parse --abbrev-ref HEAD)
     )
-    BEGIN {        
-        if  ( !( Test-Path -Path $QaRepo -PathType "Container" ) ) {
-            
+    BEGIN {
+        if  ( !( Test-Path -Path $QaRepo -PathType "Container" ) ) {            
             Write-Verbose "Create QA repository folder in: $QaRepo"
-            New-Item -Path $QaRepo -ItemType "Container" -ErrorAction Stop -ForegroundColor Green
+            New-Item -Path $QaRepo -ItemType "Container" -ErrorAction Stop
         
-            if ( !( Test-Path -Path $QaRepo/$branch -PathType "Container" ) ) {
-                Write-Verbose "Create story folder $branch in $QaRepo"
-                New-Item -Path $QaRepo/$branch -ItemType "Container" -ErrorAction Stop -ForegroundColor Green
-            }
         }
+        
+        if ( !( Test-Path -Path $QaRepo/$branch -PathType "Container" ) ) {
+            Write-Verbose "Create story folder $branch"
+            New-Item -Path $QaRepo/$branch -ItemType "Container" -ErrorAction Stop
+        }
+
         $node = Get-Process node -ErrorAction SilentlyContinue
         if ( $node ) {
             Write-Verbose "Stopping node process"
             $node | Stop-Process -Force
         }
     }
-    PROCESS {
+    PROCESS {        
         npm run build
         cp build/* $QaRepo/$branch
-        cd $QaRepo/; git add .; git commit -m "updates"; git push origin master;
-        cd $SourceRepo       
+        cd $QaRepo; git add .; git commit -m "updates for $branch"; git push origin master;
+        cd $SourceRepo
     }
     END {
         Write-Host "Deployed at https://wmorrison-frontlineed.github.io/fcqa/$branch" -ForegroundColor Blue
