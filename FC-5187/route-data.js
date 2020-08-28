31df6b0d9b38183de97eed5af61b6625b9133bd6
Will Morrison <wmorrison@frontlineed.com>
2020-08-28 15:51:34 -0400

    updates for FC-5187

---
+++ FC-5187/route-data.js
@@ -0,0 +1,96 @@
+'use strict';
+
+registerRouteData('ec-employee', [{
+    stateName: 'employeeRecord.credentials',
+    url: '/credentials',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord.emergency',
+    url: '/emergency{action:.*}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'edit') || permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord',
+    url: '/record/{employeeId:[0-9A-Za-z-]+}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord.employment',
+    url: '/employment',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'edit') || permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord.medical',
+    url: '/medical',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'edit') || permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord.personal',
+    url: '/personal{action:.*}',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'edit') || permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'employeeRecord.personnel',
+    url: '/personnel',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'edit') || permissionSvc.isPermittedAction('employees', 'view');
+    }
+}, {
+    stateName: 'index',
+    url: '/',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('bundle', 'viewothers');
+    }
+}, {
+    stateName: 'myProfile',
+    url: '/my-profile',
+    $permissions: function $permissions(permissionSvc) {
+        return !permissionSvc.isPermittedAction('bundle', 'viewothers');
+    }
+}, {
+    stateName: 'reports.assignments',
+    url: '/assignments',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('assignmentReports', 'view');
+    }
+}, {
+    stateName: 'reports.assignments.report',
+    url: '/report'
+}, {
+    stateName: 'reports.assignments.settings',
+    url: '/settings/:templateId',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('exportTemplates', 'view');
+    }
+}, {
+    stateName: 'reports.credentials',
+    url: '/credentials',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employeecredentials', 'viewreports');
+    }
+}, {
+    stateName: 'reports.demographic',
+    url: '/demographic',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('employees', 'viewreports');
+    }
+}, {
+    stateName: 'reports.demographic.report',
+    url: '/report'
+}, {
+    stateName: 'reports.demographic.settings',
+    url: '/settings/:templateId',
+    $permissions: function $permissions(permissionSvc) {
+        return permissionSvc.isPermittedAction('exportTemplates', 'view');
+    }
+}, {
+    stateName: 'reports',
+    url: '/reports'
+}]);
